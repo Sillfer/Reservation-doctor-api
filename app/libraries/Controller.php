a2c5a3b617ef5309f08d52bb14d4203eae15f18f
Sillfer <gliouinemahdi0@gmail.com>
2022-03-30 23:05:08 +0000

    addClient

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,15 +1,15 @@
 <?php
 class Controller{
-    public function model($model){
-        if(file_exists(APPROOT . "/models/" . $model . ".php")){
-            require_once APPROOT . "/models/" . $model . ".php";
-            return new $model;
-        }
+    public function model($model){  
+        if(file_exists(APPROOT . "/models/" . $model . ".php")){    // Check if the model file exists
+            require_once APPROOT . "/models/" . $model . ".php";    // Require the model file
+            return new $model;                                   // Return the model
+        }   
     }
 
-    public function view($view, $data = []){
-        if(file_exists(APPROOT . "/views/" . $view . ".php")){
-            require_once APPROOT . "/views/" . $view . ".php";
+    public function view($view, $data = []){    // $data is an array of data to be passed to the view
+        if(file_exists(APPROOT . "/views/" . $view . ".php")){  // Check if the view file exists
+            require_once APPROOT . "/views/" . $view . ".php";  // Require the view file
         }else {die("view doesn't exist!!!");}
     }
 }
